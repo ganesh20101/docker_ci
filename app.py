a2345e184725c43f034426f45aebc8a8e6c449db
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2024-12-23 17:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("Hello, cloud-engine labs!!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    # Make sure to bind to all interfaces (0.0.0.0) so it's accessible outside the container
+    app.run(host='0.0.0.0', port=8080)
